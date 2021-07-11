71157fb3dbe95b8b308e1a946347b73b3ad840bc
Erick Silva <erick.ap.silva@gmail.com>
2021-07-11 20:27:23 -0300

    Category

--- src/main/java/com/udemycourse/course/config/TestConfig.java
+++ src/main/java/com/udemycourse/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.udemycourse.course.entities.Category;
 import com.udemycourse.course.entities.Order;
 import com.udemycourse.course.entities.User;
 import com.udemycourse.course.entities.enums.OrderStatus;
+import com.udemycourse.course.repositories.CategoryRepository;
 import com.udemycourse.course.repositories.OrderRepository;
 import com.udemycourse.course.repositories.UserRepository;
 
@@ -20,6 +22,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@Autowired
 	private OrderRepository orderRepository;
@@ -34,8 +39,13 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2021-07-20T18:15:00Z"), u2, OrderStatus.WAITING_PAYMENT);
 		Order o3 = new Order(null, Instant.parse("2021-07-20T19:10:00Z"), u1, OrderStatus.CANCELED);
 		
+		Category c1 = new Category(null, "Books");
+		Category c2 = new Category(null, "Games");
+		Category c3 = new Category(null, "Comics");
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 	
 	
